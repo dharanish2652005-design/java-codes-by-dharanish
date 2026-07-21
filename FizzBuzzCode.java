128aca180194067d138e294ef38d08fbb4a7efd9
dharanish2652005-design <dharanish2652005@gmail.com>
2026-07-21 15:11:19 +0530

    Create FizzBuzzCode.java

---
+++ FizzBuzzCode.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[]args) {
+    int i = 1;
+    while(i<=50){
+        if(i%5==0&i%3==0){
+        System.out.println("FIZZ BUZZ");
+        i++;
+    } 
+    else if(i%3==0){
+        System.out.println("FIZZ");
+        i++;
+    }
+    else if(i%5==0){
+        System.out.println("BUZZ");
+        i++;
+    }
+    else{
+        System.out.println(i);
+        i++;}
+    }
+  }
+}
