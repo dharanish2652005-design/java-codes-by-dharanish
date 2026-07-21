0245c5b0f10064e53dfac62fc513b10c39ee303f
dharanish2652005-design <dharanish2652005@gmail.com>
2026-07-21 15:34:42 +0530

    Create PrintingAllElementsCode.java

---
+++ PrintingAllElementsCode.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] arr = {
+            {6, 6, 6},
+            {3, 3, 3},
+            {9, 9, 9}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
